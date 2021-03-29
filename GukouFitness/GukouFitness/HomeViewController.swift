bcbb0cd98e52a3c9ffb2d8e0f6d5b5c0ea2e36a2
Gukou4869 <st11121.grtkmch@gmail.com>
2021-03-30 08:39:33 +0900

    Created piechart

--- GukouFitness/GukouFitness/HomeViewController.swift
+++ GukouFitness/GukouFitness/HomeViewController.swift
@@ -15,8 +15,15 @@ class HomeViewController: UIViewController {
         gradientLayer.frame = self.view.bounds
         gradientLayer.colors = [UIColor.yellow.cgColor, UIColor.white.cgColor]
         self.view.layer.insertSublayer(gradientLayer, at: 0)
-        self.title = "item"
-        self.tabBarItem.image = UIImage(named: "home")
-        self.tabBarItem.selectedImage = UIImage(named: "item_selected")
+        
+        //let's start by drawing a circle a circle somehow
+        
+        let shapeLayer = CAShapeLayer()
+        
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
+        shapeLayer.path = circularPath.cgPath
+        
+        view.layer.addSublayer(shapeLayer)
     }
 }
