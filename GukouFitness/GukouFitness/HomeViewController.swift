22cd660806bd32efae16256eaa388a60832aaf16
Gukou4869 <st11121.grtkmch@gmail.com>
2021-03-31 21:52:16 +0900

    Created icon for tabbar

--- GukouFitness/GukouFitness/HomeViewController.swift
+++ GukouFitness/GukouFitness/HomeViewController.swift
@@ -25,7 +25,6 @@ class HomeViewController: UIViewController {
         let center = view.center
         let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: 0, endAngle: 2*CGFloat.pi, clockwise: true)
         shapeLayer.path = circularPath.cgPath
-        
         shapeLayer.strokeColor = UIColor.red.cgColor
         shapeLayer.lineWidth = 10
         
