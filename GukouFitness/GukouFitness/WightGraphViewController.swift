3f50aceddc577d3cef0237da0501b91428d196a6
Gukou4869 <st11121.grtkmch@gmail.com>
2021-03-31 21:42:40 +0900

    Connected storyboard with files

---
+++ GukouFitness/GukouFitness/WightGraphViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WightGraphViewController.swift
+//  GukouFitness
+//
+//  Created by 谷口翔太 on 2021/03/31.
+//
+
+import UIKit
+
+class WightGraphViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
